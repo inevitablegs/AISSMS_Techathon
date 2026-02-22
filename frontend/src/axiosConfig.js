9cf16646fdf5cb62a7057d28f0d4254f28961c13
Durvankur-Joshi <joshidurvankur.29@gmail.com>
2026-02-22 13:56:21 +0530

    update

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://hackathon-gcoeara.onrender.com/',
+    baseURL: 'https://hackathon-gcoeara.onrender.com/auth/api',
     headers: {
         'Content-Type': 'application/json',
     },
