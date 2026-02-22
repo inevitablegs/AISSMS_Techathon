41e1c561bf85ca12e40ca7296a78ae6be2e1b948
Durvankur-Joshi <joshidurvankur.29@gmail.com>
2026-02-22 14:21:29 +0530

    update

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -136,6 +136,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:5174",
     "http://127.0.0.1:5174",
+    "https://hackathon-gcoeara.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
